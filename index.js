13e8ef294ce0e22597ff810bc727296354359716
Karan Modi <kpmodi@my.waketech.edu>
2025-01-13 14:00:14 -0500

    Added everything

---
+++ index.js
@@ -0,0 +1,34 @@
+// JavaScript Code goes here
+console.log("Internal JS console message. Hello, everyone")
+console.log(document)
+// console.dir(document)
+
+
+// ALERTS
+// alert("Hello world") // Textbot pop-up
+window.onload = function(){
+    // alert(`Hello java`)
+}
+// 
+
+// PROMPTS //
+
+// const student = prompt("What is tu name?")
+//console.log(student)
+// writes to document, but doesnt work with script deger
+//document.write(`<p>Greetings from outer space, ${student}</p>`)
+
+//document.getElementById("myH1").innerHTML = "I like javaScript";
+//document.getElementById("myParagraph").textContent = "Hello"
+///
+
+const student = prompt("what is your name?") // Create a new <p> element
+const messageElement = document.createElement("p"); // sets the text content of that
+messageElement.textContent = `Hello, ${student} ` // append the new <p> to body
+document.body.appendChild(messageElement); // Append the new <p> to body
+// is ensures conent is dynamically added
+
+// Styling page elements
+document.body.style.color = "green"
+document.body.style.backgroundColor = "black"
+document.body.style.fontSize = "1.6em"
